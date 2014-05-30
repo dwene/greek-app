32488df71ca0722dcf47475ca722eee212bcfdd9
Derek Wene <djrobotfreak@gmail.com>
2014-05-30 12:39:02 -0500

    got rid of dumb function

--- js/main.js
+++ js/main.js
@@ -11,52 +11,6 @@ var PERMS_LIST =  [ALUMNI, MEMBER, LEADERSHIP, COUNCIL];
 //initialize app
 var App = angular.module('App', ['ngRoute']);
 
-    App.filter('multiSearch', function() { //provides single-search-box multi-term search functionality
-        return function (objects, searchValues, delimiter) {
-            if (!delimiter) {
-                delimiter="";
-            }
-            if (searchValues) {
-                var good = Array(0); //the list of objects that match ALL terms
-                var terms = String(searchValues).toUpperCase().split(delimiter); 
-                for (var w = 0; w < terms.length; w++) {
-                    terms[w] = terms[w].replace(/^\s+|\s+$/g,"");
-                }
-                var truthArray = Array(terms.length); //the truth array matches 1 to 1 to the terms. If an element in the truthArray is 0, the corresponding term wasn’t found
-                for (var j = 0; j < objects.length; j++) { //iterates through each object
-                    for (var t = 0; t < objects.length; t++) {
-                        truthArray[t] = 0; //initializes/resets the truthArray
-                    }
-                    for (var i = 0; i < terms.length; i++) {
-                        if (objects[j].attribute1) {
-                            if (String(objects[j].attribute1).toUpperCase().indexOf(terms[i]) != -1) {
-                                truthArray[i] = 1;
-                            }
-                        }
-                        if (truthArray[i] != 1 && objects[j].attribute2) {
-                            if (String(objects[j].attribute2).toUpperCase().indexOf(terms[i]) != -1) {
-                                truthArray[i] = 1;
-                            }
-                        }
-                        if (truthArray[i] != 1 && objects[j].attribute3) {
-                            if (String(objects[j].attribute3).toUpperCase().indexOf(terms[i]) != -1) {
-                                truthArray[i] = 1;
-                            }
-                        }
-                    }
-                    if (truthArray.indexOf(0) == -1) { //if there are no 0s, all terms are present and the object is good
-                        good.push(objects[j]); //add the object to the good list
-                    }
-                }
-                return good; //return the list of matching objects
-            }
-            else { //if there are no terms, return all objects
-                return objects;
-            }
-        }
-    });
-
-
     App.factory('formDataObject', function() {
             return function(data) {
                 var fd = new FormData();
