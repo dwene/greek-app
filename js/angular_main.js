5c6764c6e4b1b8e0273fc3cf6b49d93ccd99de38
Jake Ruesink <jaruesink@gmail.com>
2014-06-24 10:42:43 -0500

    added a ton of pdf report stuff

--- js/angular_main.js
+++ js/angular_main.js
@@ -2264,6 +2264,29 @@ App.controller('eventCheckInReportController', function($scope, $http, Load, $st
         $scope.loading = true;
         Load.then(function(){
             
+            $scope.createReport = function(){
+            
+                var doc = new jsPDF();
+
+                // We'll make our own renderer to skip this editor
+                var specialElementHandlers = {
+                    '#editor': function(element, renderer){
+                        return true;
+                    }
+                };
+
+                // All units are in the set measurement for the document
+                // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
+                doc.fromHTML($('#report').get(0), 15, 15, {
+                    'width': 170, 
+                    'elementHandlers': specialElementHandlers
+                });
+                
+                doc.output('dataurlnewwindow');
+
+                
+            }
+            
         });
   });
 
