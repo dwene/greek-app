2ae0a60d413b04913a3e8c356178f040c31de958
Jake Ruesink <jaruesink@gmail.com>
2014-07-04 12:25:42 -0500

    design updates for tagging

--- js/angular_main.js
+++ js/angular_main.js
@@ -2886,15 +2886,47 @@ App.directive('typeAhead', function($compile){
     return{
         restrict: 'E',
         replace: 'true',
-        template: '<div></div>',
+        template: '<div class="form-group"></div>',
         link: function(scope, element, attrs){
         
         var placeholder = attrs.placeholder;
         var this_id = attrs.id;
   
                 element.append(
-                '<input type="text" class="form-control typeahead" placeholder="'+placeholder+'">'
-                );
+                '<input type="text" class="form-control typeahead" autocomplete="off" placeholder="'+placeholder+'">'
+                    +'<script type="text/javascript">'
+                    +'var substringMatcher = function(strs) {'
+                    +'return function findMatches(q, cb) {'
+                    +'var matches, substringRegex;'
+                    +'matches = [];'
+                    +"substrRegex = new RegExp(q, 'i');"
+                    +'$.each(strs, function(i, str) {'
+                    +'if (substrRegex.test(str)) {'
+                    +'matches.push({ value: str });'
+                    +'}});'
+                    +'cb(matches);'
+                    +'};};'
+                    +"var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',"
+                    +  "'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',"
+                    +  "'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',"
+                    +  "'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',"
+                    +  "'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',"
+                    +  "'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',"
+                    +  "'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',"
+                    +  "'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',"
+                    +  "'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'"
+                    +'];'
+                    +"$('.typeahead').typeahead({"
+                    + 'hint: true,'
+                    +  'highlight: true,'
+                    +  'minLength: 1'
+                    +'},{'
+                    +  "name: 'states',"
+                    +  "displayKey: 'value',"
+                    +  'source: substringMatcher(states)'
+                    +'});'
+                    +'</script>'
+);
                         
             
             
