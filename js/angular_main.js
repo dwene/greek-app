ee5938b20a4f7df320d1eaca42a2d863d0f34bb7
Derek Wene <djrobotfreak@gmail.com>
2014-08-07 13:03:14 -0500

    remove http stuff

--- js/angular_main.js
+++ js/angular_main.js
@@ -3668,6 +3668,29 @@ App.directive('match', function(){
         };
 });
 
+App.directive('removeHttp', function(){
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        scope: {
+            ngModel: '='
+        },
+        controller: function($scope) {
+            var index = 0;
+            $scope.$watch('ngModel', function(){
+                if ($scope.ngModel){
+                    if ($scope.ngModel.slice(0, 7).toLowerCase() == 'http://'){
+                        $scope.ngModel = $scope.ngModel.slice(7);
+                    }
+                    if ($scope.ngModel.slice(0, 8).toLowerCase() == 'https://'){
+                        $scope.ngModel = $scope.ngModel.slice(8);
+                    }
+                }
+
+            })
+        }};
+});
+
 App.directive('listThing', function(){
         return {
             require: 'ngModel',
