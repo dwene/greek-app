7f041c86024805a360c697a56561f011bf1aacd5
Derek Wene <djrobotfreak@gmail.com>
2014-08-31 22:56:17 -0500

    fixed the report error modal submission

--- js/angular_main.js
+++ js/angular_main.js
@@ -524,12 +524,27 @@ App.config(function($stateProvider, $urlRouterProvider) {
                 window.location.assign('#/app/home');
             }
         }
+        $scope.sendHelpMessage = function(isValid, content){
+            console.log('I am getting submitted');
+            if(isValid){
+                $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
+                .success(function(){console.log('success');})
+                .error(function(){console.log('error');})
+            }
+            else{
+            //do nothing
+            }
+        }
 	});
 
     App.controller('helpMessageController', function($scope, $http, $rootScope) {
-        $scope.sendHelpMessage = function(isValid){        
+        $scope.howdy="howdy";
+        $scope.sendHelpMessage = function(isValid, content){
+            console.log('I am getting submitted');
             if(isValid){
-            //send the help message
+                $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
+                .success(function(){console.log('success');})
+                .error(function(){console.log('error');})
             }
             else{
             //do nothing
