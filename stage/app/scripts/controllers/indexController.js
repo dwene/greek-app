797ca98d585a8731501ccdf0ba7c5e55fe5f3e82
Derek Wene <djrobotfreak@gmail.com>
2014-12-07 14:46:55 -0600

    dialog controller works

--- stage/app/scripts/controllers/indexController.js
+++ stage/app/scripts/controllers/indexController.js
@@ -34,4 +34,24 @@
                     templateUrl: '../views/templates/helpDialog.html'
             });
         }
+        function dialogController($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
+            $scope.sendHelpMessage = function(isValid, content){
+                console.log('I am getting submitted');
+                if(isValid){
+                    $scope.sendingHelp='pending';
+                    $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
+                    .success(function(){console.log('success');
+                    $scope.helpMessage.$setPristine();
+                    $scope.message={};
+                    $scope.sendingHelp='done';
+                    $mdDialog.hide();
+                    })
+                    .error(function(){console.log('error');
+                    $scope.sendingHelp='broken';})
+                }
+                else{
+                //do nothing
+                }
+            }
+}
 	});
