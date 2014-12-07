26e1e04867aeaf1b08377a3010a61e169aaaadea
Derek Wene <djrobotfreak@gmail.com>
2014-12-07 14:58:21 -0600

    fixed the dialog controller stuff

---
+++ stage/app/scripts/controllers/dialogController.js
@@ -0,0 +1,22 @@
+console.log('hi i am the dialog controller');
+App.controller('dialogController', function dialogController($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
+
+            $scope.sendHelpMessage = function(isValid, content){
+                console.log('I am getting submitted');
+                if(isValid){
+                    $scope.sendingHelp='pending';
+                    $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
+                    .success(function(){console.log('success');
+                    $scope.helpMessage.$setPristine();
+                    $scope.message={};
+                    $scope.sendingHelp='done';
+                    $mdDialog.hide();
+                    })
+                    .error(function(){console.log('error');
+                    $scope.sendingHelp='broken';})
+                }
+                else{
+                //do nothing
+                }
+            }
+});
--- stage/app/scripts/controllers/indexController.js
+++ stage/app/scripts/controllers/indexController.js
@@ -1,4 +1,6 @@
-    App.controller('indexController', function($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
+   
+console.log('hi i am the index controller');
+App.controller('indexController', function($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
         $scope.homeButton = function(){
             if ($rootScope.checkAlumni()){
                 window.location.assign('#/app/directory/members');
@@ -30,28 +32,30 @@
         }
         $scope.showHelpdialog = function(){
             $mdDialog.show({
-                    controller: dialogController,
+                    controller: 'dialogController',
                     templateUrl: '../views/templates/helpDialog.html'
             });
         }
-        function dialogController($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
-            $scope.sendHelpMessage = function(isValid, content){
-                console.log('I am getting submitted');
-                if(isValid){
-                    $scope.sendingHelp='pending';
-                    $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
-                    .success(function(){console.log('success');
-                    $scope.helpMessage.$setPristine();
-                    $scope.message={};
-                    $scope.sendingHelp='done';
-                    $mdDialog.hide();
-                    })
-                    .error(function(){console.log('error');
-                    $scope.sendingHelp='broken';})
-                }
-                else{
-                //do nothing
-                }
-            }
-}
+
 	});
+
+// App.controller('dialogController', function dialogController($scope, $http, LoadScreen, $rootScope, $timeout, $mdSidenav, $mdDialog) {
+//             $scope.sendHelpMessage = function(isValid, content){
+//                 console.log('I am getting submitted');
+//                 if(isValid){
+//                     $scope.sendingHelp='pending';
+//                     $http.post(ENDPOINTS_DOMAIN + '/_ah/api/netegreek/v1/user/report_error', packageForSending(content))
+//                     .success(function(){console.log('success');
+//                     $scope.helpMessage.$setPristine();
+//                     $scope.message={};
+//                     $scope.sendingHelp='done';
+//                     $mdDialog.hide();
+//                     })
+//                     .error(function(){console.log('error');
+//                     $scope.sendingHelp='broken';})
+//                 }
+//                 else{
+//                 //do nothing
+//                 }
+//             }
+// });
