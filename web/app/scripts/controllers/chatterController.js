b2870935e5c86a771f81d05d9386b04023db5818
Jake Ruesink <jaruesink@gmail.com>
2015-06-14 01:14:53 -0500

    chatter stuff

--- web/app/scripts/controllers/chatterController.js
+++ web/app/scripts/controllers/chatterController.js
@@ -2,11 +2,52 @@ App.controller('chatterController', ['$scope', 'RESTService', '$rootScope', '$md
                                      
 function($scope, RESTService, $rootScope, $mdDialog, $timeout, localStorageService, Directory) {
     
+    $scope.like = false;
     
-    $scope.chatter = [
-        {content:'howdy'},
+    $scope.chatters = [
+        {content:'Hey everybody, I was wondering what we were planning for the party tomorrow. Comment here if you want to help me plan! Hey everybody, I was wondering what we were planning for the party tomorrow. Comment here if you want to help me plan! Hey everybody, I was wondering what we were planning for the party tomorrow. Comment here if you want to help me plan!'},
         {content:'testing'}
     ];
+    
+    $scope.likeChatter = function(chat){
+    if ($scope.like == false){
+        $scope.like = true;
+    }
+        else{
+        $scope.like = false;    
+    }
+    };
+    
+    $scope.makeImportant = function(chat){
+        //somehow make this chat important
+    }
+    $scope.openChatter = function(chat){
+        $scope.chat = chat;
+        $mdDialog.show({
+                    controller: ('chatterDialogController', ['$scope', '$mdDialog', chatterDialogController]),
+                    templateUrl: 'views/templates/chatterDialog.html'
+        });
+    }
+    
+     function chatterDialogController(scope, mdDialog){
+         scope.chat = $scope.chat;
+            scope.hide = function(){
+                mdDialog.hide();
+            }
+        }
+    
+    $scope.newChatter = function(){
+        $mdDialog.show({
+                    controller: ('newChatterController', ['$scope', '$mdDialog', newChatterController]),
+                    templateUrl: 'views/templates/newChatterDialog.html'
+        });
+    }
+    
+    function newChatterController(scope, mdDialog){
+            scope.hide = function(){
+                mdDialog.hide();
+            }
+        }
     
 }
                                      
