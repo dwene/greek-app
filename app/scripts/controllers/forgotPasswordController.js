a7a4d90a4096d033b5b9478083bdead00a66b39d
jaruesink <jaruesink@gmail.com>
2015-08-02 19:40:07 -0500

    reset password page looks nicer now

--- app/scripts/controllers/forgotPasswordController.js
+++ app/scripts/controllers/forgotPasswordController.js
@@ -1,46 +1,49 @@
 App.controller('forgotPasswordController', ['$scope', 'RESTService', '$rootScope',
-    function($scope, RESTService, $rootScope) {
-        routeChange();
-        $scope.sentEmail = false;
-        $scope.reset = function(input) {
-            $scope.gettingnewPassword = 'pending';
-
-            function validEmail(v) {
-                var r = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-                return (v.match(r) == null) ? false : true;
-            }
-            if (validEmail(input)) {
-                to_send = {
-                    email: input,
-                    user_name: ''
-                };
-            } else {
-                to_send = {
-                    email: '',
-                    user_name: input.toLowerCase()
-                };
-            }
-            console.log(to_send);
-            RESTService.post(ENDPOINTS_DOMAIN + '/_ah/api/auth/v1/forgot_password', to_send)
-                .success(function(data) {
-                    if (!RESTService.hasErrors(data)) {
-                        if (data.data == 'OK') {
-                            $scope.sentEmail = true;
-                            $scope.gettingnewPassword = 'done';
-                        } else {
-                            $scope.returned_choices = JSON.parse(data.data);
-                            $scope.showList = true;
-                        }
-                        $scope.gettingnewPassword = 'done';
-                    } else {
-                        $scope.emailFailed = true;
-                        $scope.gettingnewPassword = 'broken';
-                    }
-                })
-                .error(function(data) {
-                    console.log('Error: ', data);
-                    $scope.emailFailed = true;
-                });
-        }
+function($scope, RESTService, $rootScope) {
+  routeChange();
+  $scope.sentEmail = false;
+  $scope.reset = function(input) {
+    $scope.gettingnewPassword = 'pending';
+    $scope.emailFailed = false;
+    $scope.showList = false;
+    $scope.sentEmail = false;
+    
+    function validEmail(v) {
+      var r = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+      return (v.match(r) === null) ? false : true;
     }
+    if (validEmail(input)) {
+      to_send = {
+        email: input,
+        user_name: ''
+      };
+    } else {
+      to_send = {
+        email: '',
+        user_name: input.toLowerCase()
+      };
+    }
+    console.log(to_send);
+    RESTService.post(ENDPOINTS_DOMAIN + '/_ah/api/auth/v1/forgot_password', to_send)
+    .success(function(data) {
+      if (!RESTService.hasErrors(data)) {
+        if (data.data == 'OK') {
+          $scope.sentEmail = true;
+          $scope.gettingnewPassword = 'done';
+        } else {
+          $scope.returned_choices = JSON.parse(data.data);
+          $scope.showList = true;
+        }
+        $scope.gettingnewPassword = 'done';
+      } else {
+        $scope.emailFailed = true;
+        $scope.gettingnewPassword = 'broken';
+      }
+    })
+    .error(function(data) {
+      console.log('Error: ', data);
+      $scope.emailFailed = true;
+    });
+  };
+}
 ]);
