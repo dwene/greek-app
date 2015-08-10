d9c35ed3cdd2e42f0fe23d45b0b5dad8172ff755
jaruesink <jaruesink@gmail.com>
2015-08-10 14:04:21 -0500

    checking in my edits to ngEnter

--- app/scripts/directives/ngEnterDirective.js
+++ app/scripts/directives/ngEnterDirective.js
@@ -1,15 +1,38 @@
 //credit: http://stackoverflow.com/questions/15417125/submit-form-on-pressing-enter-with-angularjs
 App.directive('ngEnter', function() {
     return function(scope, element, attrs) {
+        var map = {
+            13: false,
+            16: false
+        }
         element.bind("keydown", function(event) {
-            if (event.which === 13 && event.which !== 16) {
-                scope.$apply(function() {
-                    scope.$eval(attrs.ngEnter, {
-                        'event': event
+            if (event.keyCode in map) {
+                map[event.keyCode] = true;
+                if (map[13] && map[16]) {
+                    //do nothing
+                }
+                if (map[13] && !map[16]) {
+                    scope.$apply(function() {
+                        scope.$eval(attrs.ngEnter, {
+                            'event': event
+                        });
                     });
-                });
-                event.preventDefault();
+                    event.preventDefault();
+                    console.log(map)
+                }
             }
         });
-    };
+        element.bind("keyup", function(event) {
+            if (event.keyCode in map) {
+                map[event.keyCode] = true;
+                console.log(map)
+                if (map[16]) {
+                    map = {
+                        13: false,
+                        16: false
+                    }
+                }
+            }
+        });
+    }
 });
