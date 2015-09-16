5e1a09c1ac488be0ffccfea7e4425511ad611532
jaruesink <jaruesink@gmail.com>
2015-09-15 23:59:06 -0500

    working towards displaying invited count for selecting members

--- app/scripts/directives/selectingMembersDirective.js
+++ app/scripts/directives/selectingMembersDirective.js
@@ -96,7 +96,7 @@ function() {
       // }
 
       function mergeMembers(a, b) {
-        usersDictionary = {}
+        usersDictionary = {};
         if (a && b){
           for (i = 0; i < a.length; i++){
             usersDictionary[a[i].key] = a[i];
@@ -106,11 +106,11 @@ function() {
           }
         }
         var mergedMembers = [];
-        for (user in usersDictionary){
+        for (var user in usersDictionary){
           mergedMembers.push(usersDictionary[user]);
         }
         return mergedMembers;
-      };
+      }
 
       $scope.showInvitedMembersDialog = function(){
         $mdDialog.show({
@@ -120,7 +120,8 @@ function() {
       };
 
       function invitedMembersDialogController(scope, mdDialog){
-        scope.members = mergeMembers($scope.selectedIndividuals, $scope.selectedCalendar.users);
+        $scope.invitedMembers = mergeMembers($scope.selectedIndividuals, $scope.selectedCalendar.users);
+        scope.members = $scope.invitedMembersDialog;
         //convert back to a list.
 
         scope.hide = function(){
@@ -288,6 +289,7 @@ function() {
           }
           return retList;
         }
+
         scope.save = function() {
           //clear selection indicators
           for (i = 0; i < scope.members.length; i++){
@@ -298,7 +300,7 @@ function() {
           }
           $scope.selectedIndividuals = $scope.customCalendar.users;
           $scope.selectedCalendar = $scope.customCalendar.calendar;
-          if ($scope.selectedIndividuals.length == 0){
+          if ($scope.selectedIndividuals.length === 0){
             $scope.internalSelectedCalendar = $scope.customCalendar.calendar;
           }
           mdDialog.hide();
